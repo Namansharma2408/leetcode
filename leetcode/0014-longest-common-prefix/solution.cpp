eb630cb8b2be59beada8535e7c00a755464e0efd
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2024-07-02 06:21:26 +0000

    Sync LeetCode submission Runtime - 3 ms (10.44%), Memory - 10.8 MB (100.00%)

---
+++ leetcode/0014-longest-common-prefix/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if( strs.size()==1 ) return strs[0];
+        string s = "";
+        int minsize = 0;
+        for( int i = 0 ; i < strs.size(); i++)if( minsize < strs[i].size()) minsize = strs[i].size();
+        for( int i = 0; i<minsize ; i++){
+            bool b = true;
+            for( int j = 1; j< strs.size() ; j++ ){
+                if( strs[0][i] != strs[j][i]) b = false;
+            }
+            if( b == true) s+= strs[0][i];
+            else return s;
+        }
+        return s;
+    }
+
+};
