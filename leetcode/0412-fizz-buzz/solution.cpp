52c5a0001464fd7f7773a23db0535e5e6dcd63da
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2024-07-07 07:20:33 +0000

    Sync LeetCode submission Runtime - 6 ms (0.41%), Memory - 10.3 MB (99.68%)

---
+++ leetcode/0412-fizz-buzz/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector <string> v;
+         
+        for( int i = 0 ; i < n ; i++){
+            if( (i+1)%15 == 0) v.push_back("FizzBuzz");
+            else if( (i+1)%5 == 0) v.push_back("Buzz");
+            else if( (i+1)%3 == 0) v.push_back("Fizz");
+            else{
+                string s = to_string(i+1);
+                v.push_back(s);
+            }
+        }
+        return v;
+    }
+};
