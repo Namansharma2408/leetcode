ed27dcf77442e68e10a2a0ae1b3d121363a7be91
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2024-07-07 09:27:09 +0000

    Sync LeetCode submission Runtime - 8 ms (17.00%), Memory - 12.1 MB (100.00%)

---
+++ leetcode/1580-shuffle-the-array/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> v(2*n,0);
+        for( int i = 0 ; i < 2*n ; i +=2){
+            v[i] = (nums[i/2]);
+            v[i + 1] = (nums[i/2 + n ]);
+        }
+        return v;
+    }
+};
