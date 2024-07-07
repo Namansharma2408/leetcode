32c64e61a0af1e626ded0b8cd9f9f75fb556ac58
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2024-07-07 09:43:26 +0000

    Sync LeetCode submission Runtime - 2 ms (3.74%), Memory - 7.2 MB (100.00%)

---
+++ leetcode/1610-xor-operation-in-an-array/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int b = 0;
+        for( int i=0  ; i< n; i++){
+            b = b^(start+2*i);
+        }
+        return b;
+    }
+};
