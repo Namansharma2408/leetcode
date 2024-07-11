80a628a02df4183a7b8b06148d7d685339de8d68
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2024-07-11 13:14:02 +0000

    Sync LeetCode submission Runtime - 16 ms (6.12%), Memory - 10.5 MB (20.23%)

--- leetcode/0009-palindrome-number/solution.cpp
+++ leetcode/0009-palindrome-number/solution.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if( x < 0 ) return false;
-       string s = to_string(x);
-       int n = s.size();
-        bool b = true;
-       for( int i = 0 ; i < n/2 ; i++){
-        if( s[i] != s[n-1-i])  b = false;
+        string s = to_string(x);
+        int n = s.size();
+        for( int i = 0 ; i < n/2 ; i++){
+            if( s[i] != s[n-1-i])  return false;
        } 
-       if( b == true) return true;
-       else return false;
+        return true;
     }
 };
