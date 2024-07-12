5806b33b8fa619c64525d9c7deb29d522bd637ab
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2024-07-12 03:23:54 +0000

    Sync LeetCode submission Runtime - 28 ms (3.05%), Memory - 28.6 MB (100.00%)

---
+++ leetcode/0495-teemo-attacking/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int x = 0;
+        for( int i = 1 ; i < timeSeries.size() ; i++){
+            x += min((timeSeries[i] - timeSeries[i-1]) , duration );
+        }
+        return (x + duration);
+    }
+};
