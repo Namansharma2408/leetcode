a2b2d95f7f748be145c9a04478718ddeb2da65e5
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2024-07-14 08:18:08 +0000

    Sync LeetCode submission Runtime - 6 ms (7.95%), Memory - 8.4 MB (99.99%)

---
+++ leetcode/0029-divide-two-integers/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if( dividend == -2147483648 && divisor == -1 ) return 2147483647;
+        if( dividend == 0) return 0;       
+        else return dividend/divisor  ;
+
+    }
+};
