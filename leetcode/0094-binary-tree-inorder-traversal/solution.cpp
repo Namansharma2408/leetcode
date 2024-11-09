10de70ec73d89976675cd049ecb7b957fd4eebaf
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2024-11-09 18:19:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (100.00%)

---
+++ leetcode/0094-binary-tree-inorder-traversal/solution.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void In(vector<int> &v , TreeNode* root){
+        if( root == NULL) return;      
+        In(v,root->left);
+        v.push_back(root->val);
+        In(v,root->right);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> v;
+        In(v,root);
+        return v;
+    }
+};
