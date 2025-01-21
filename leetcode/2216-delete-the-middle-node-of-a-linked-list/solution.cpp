1487344095a28bef4ba52e66278acc5d5916a2e5
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-01-21 10:57:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 311.9 MB (82.86%)

---
+++ leetcode/2216-delete-the-middle-node-of-a-linked-list/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if( head == NULL) return NULL;
+        if(head->next == NULL) return NULL;
+        ListNode* temp = head;
+        int n = 0;
+        while( temp ){
+            n++;
+            temp = temp->next;
+        }
+        temp = head;
+        for( int i = 1 ; i < n/2 ; i++){
+            temp = temp->next;
+        }
+        if(temp->next)temp->next = temp->next->next;
+        return head;
+    }
+};
