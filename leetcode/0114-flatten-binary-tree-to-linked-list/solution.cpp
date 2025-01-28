2570b537f5c1cadac34f7dacb082994f5c2b91b4
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-01-28 18:13:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.5 MB (51.86%)

--- leetcode/0114-flatten-binary-tree-to-linked-list/solution.cpp
+++ leetcode/0114-flatten-binary-tree-to-linked-list/solution.cpp
@@ -1,37 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void stackPush(TreeNode* root,stack<TreeNode*> &st ){
-        if( root== NULL) return;
-        st.push(root);
-        if(root->left) stackPush(root->left,st);
-        if(root->right)stackPush(root->right,st);
-        return;
-    }
     void flatten(TreeNode* root) {
-        if( root == NULL) return;
-        if( root->left == NULL && root->right == NULL) return;
-        stack<TreeNode*> st;
-        stackPush(root,st);
-        root = st.top();
-        st.pop();
-        while(!st.empty()){
-            st.top()->right = root;
-            st.top()->left = NULL;
-            root = st.top();
-            st.pop();
+        if( root == NULL ) return ;
+        TreeNode* curr = root;
+        while( curr != NULL ){
+            if( curr->left != NULL ){
+                // save the right;
+                TreeNode* r = curr->right;
+                curr->right = curr->left;
+                // finding pred
+                TreeNode* pred = curr->left;
+                while( pred->right != NULL) pred = pred->right;
+                // link
+                pred->right = r;
+                curr = curr->left;
+            }
+            else curr = curr->right;
         }
-        root->left = NULL;
-        return;
+        TreeNode* temp = root;
+        while( temp->right != NULL ){
+            temp->left = NULL;
+            temp = temp->right;
+        }
     }
 };
