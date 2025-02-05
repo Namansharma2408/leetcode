4a8c7342448ce5ae51b692d5ef1800ba95e3d4b1
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-02-05 17:06:18 +0000

    Sync LeetCode submission Runtime - 4 ms (35.78%), Memory - 10.1 MB (10.35%)

--- leetcode/0242-valid-anagram/solution.cpp
+++ leetcode/0242-valid-anagram/solution.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-		sort(t.begin(),t.end());
-		if(s == t ) return 1;
-		else  return 0;
+        if( s.size() != t.size()) return false;
+        unordered_map<char,int> map;
+        int n = s.size();   
+        for( int i = 0 ; i < n ; i++){
+            map[s[i]]++;
+            map[t[i]]--;
+            if( map[s[i]] == 0) map.erase(s[i]);
+        }
+        for( auto i : map){
+            if( i.second != 0) return false;
+        }
+        
+        return true;
     }
 };
