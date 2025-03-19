907accfd6d1bb321ce1781d44254026a8d41a505
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-03-19 22:19:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.3 MB (27.50%)

--- leetcode/0138-copy-list-with-random-pointer/solution.cpp
+++ leetcode/0138-copy-list-with-random-pointer/solution.cpp
@@ -13,50 +13,25 @@ public:
         }
         return dummy->next;
     }
-    Node* merge(Node* l1,Node* l2){
-
-        Node* t1 = l1;
-        Node* t2 = l2;
-        Node* t11 = t1->next;
-        Node* t22 = t2->next;
-        while(t1 != NULL){
-            t1->next = t2;
-            t2->next = t11;
-            if( t11 == NULL) break;
-            t1 = t11;
-            t2 = t22;
-            t11 = t11->next;
-            t22 = t22->next;
-        }
-        return l1;
-    }
-    void randomassign(Node* d){
-        Node* temp = d;
-        while( temp != NULL && temp->next != NULL ){
-            if(temp->next != NULL && temp->random != NULL) temp->next->random = temp->random->next;
-            temp = temp->next->next;
-        }
-    }
+    
     Node* copyRandomList(Node* head) {
         if( head == NULL) return NULL;
-        Node* dummy = makedummy(head);
-        Node* d = merge(head,dummy);
-        randomassign(d);
-        Node* list1 = d;
-        Node* list2 = d->next;
-        Node* temp = d;
-        Node* temp1 = list1;
-        Node* temp2 = list2;
-        int count = 1;
-        while( temp1 != NULL && temp1->next->next != NULL){
-            temp1->next = temp1->next->next;
-            temp1 = temp1->next;
-            temp2->next = temp1->next;
-            temp2 = temp2->next;
+        Node* head2 = makedummy(head);
+        unordered_map<Node*,Node*> mp;
+        Node* tempa = head;
+        Node* tempb = head2;
+        while( tempa!= NULL){
+            mp[tempa] = tempb;
+            tempa = tempa->next;
+            tempb = tempb->next;
         }
-        temp1->next = NULL;
-        temp2->next = NULL;
-        
-        return list2;
+        for( auto x : mp){
+            Node* c = x.first;
+            Node* d = x.second;
+            if(c->random != NULL)  d->random = mp[c->random];
+            else d->random = NULL;
+            
+        }
+        return head2;
     }
 };
