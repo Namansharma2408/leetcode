0ce544c6747400cd9ccdb33065800fc89aac23ee
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-03-28 16:57:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (39.17%)

---
+++ leetcode/0274-h-index/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        int n = citations.size();
+        int lo = 0;
+        int hi = n-1;
+        int mid = 0;
+        while( lo <= hi ){
+            mid = (lo+hi)/2;
+            if( citations[mid] == n-mid) return n-mid;
+            else if(citations[mid] > n - mid ) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        cout<<lo<<" "<<hi<<endl;
+        return (n - lo);
+    }
+};
