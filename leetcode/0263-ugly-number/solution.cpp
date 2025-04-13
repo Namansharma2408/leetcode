53c07365bea841dc591c2157e934201a019de173
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-04-13 20:40:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (7.95%)

---
+++ leetcode/0263-ugly-number/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    bool isUgly(int n) {
+        if( n == 0) return false;
+        while(true){
+            if(n%2 == 0) n /= 2;
+            else break;
+        }
+        while(true){
+            if(n%3 == 0) n /= 3;
+            else break;
+        }
+        while(true){
+            if(n%5 == 0) n /= 5;
+            else break;
+        }
+        if( n == 1 ) return true;
+        else return false; 
+    }
+};
