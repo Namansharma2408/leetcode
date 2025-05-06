c85b6a885978387ae30696c8979f131f482f2f9a
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-05-06 18:31:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.2 MB (23.09%)

---
+++ leetcode/0096-unique-binary-search-trees/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,1);
+        for( int i = 1 ; i <= n ; i++){
+            int ans = 0;
+            for( int j = 0 ; j < i ; j++) ans += dp[j]*dp[i-j-1];
+            dp[i] = ans;
+        }
+        return dp[n];
+    }
+};
