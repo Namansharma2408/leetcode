ae7c677b157f5328236f8d93e154c4405d0575ca
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-05-14 17:24:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 71.8 MB (15.21%)

--- leetcode/0053-maximum-subarray/solution.cpp
+++ leetcode/0053-maximum-subarray/solution.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    int maxi(vector<int> &nums,int i , int j){
-        if( i == j ) return nums[i];
-        if( i > j ) return 0;
-        int k = (i+j)/2;
-        int left = 0;
-        int right = 0;
-        int maxLeft = INT_MIN;
-        int maxRight = INT_MIN;
-        for( int x = k ; x >= i ; x--){
-            left += nums[x];
-            maxLeft = max(maxLeft,left);
-        }
-        for( int x = k+1 ; x <= j ; x++){
-            right += nums[x];
-            maxRight = max(maxRight,right);
-        }
-        return max(maxLeft+maxRight,max(maxi(nums,i,k),maxi(nums,k+1,j)));
-
-    }
     int maxSubArray(vector<int>& nums) {
-        return maxi(nums,0,nums.size()-1);
+        int n = nums.size();
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        for( int i = 1 ; i < n ; i++){
+            currSum = max(nums[i],currSum+nums[i]);
+            maxSum = max(maxSum,currSum);
+        }
+        return maxSum;
     }
 };
