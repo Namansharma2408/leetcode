b37c0e9fb55b822e538cfc3c3e17afa998ce9bf1
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-05-15 07:08:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.8 MB (23.21%)

--- leetcode/0073-set-matrix-zeroes/solution.cpp
+++ leetcode/0073-set-matrix-zeroes/solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        for( int i = 0 ; i < n ; i++){
-            for( int j = 0 ; j < m ; j++){
+        int x = 1e9+17;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for( int i = 0 ; i < m ; i++){
+            for( int j = 0 ; j < n ; j++){
                 if(matrix[i][j] == 0){
-                    for( int k = 0 ; k < m ; k++){
-                        if(matrix[i][k]) matrix[i][k] = INT_MIN+2;
-                    }
-                    for( int k = 0 ; k < n ; k++){
-                        if(matrix[k][j]) matrix[k][j] = INT_MIN+2;
-                    }
-                    matrix[i][j] = 0;
+                    for( int k = 0 ; k < m ; k++) if(matrix[k][j] != 0) matrix[k][j] = x;
+                    for( int k = 0 ; k < n ; k++) if(matrix[i][k] != 0) matrix[i][k] = x;
                 }
             }
         }
-        for( int i = 0 ; i < n ; i++) for( int j = 0 ; j < m ; j++) if(matrix[i][j] == INT_MIN+2) matrix[i][j] = 0;
-        
+        for( int i = 0 ; i < m ; i++){
+            for( int j = 0 ; j < n ; j++){
+                if(matrix[i][j] == x) matrix[i][j] = 0;
+            }
+        }
+        return;
     }
 };
