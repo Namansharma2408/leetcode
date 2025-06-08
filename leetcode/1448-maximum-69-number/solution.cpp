7dc2a85c1f23a150914eb5429dbc3e5aea12d5dd
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-06-08 17:23:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (9.29%)

---
+++ leetcode/1448-maximum-69-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int n = num;
+        int pos = -1;
+        int i = 0;
+        while( n != 0){
+            if( n%10 == 6) pos = i;
+            n /= 10;
+            i++;
+        }
+        if( pos == -1) return num;
+        return (num + 3*pow(10,pos));
+    }
+};
