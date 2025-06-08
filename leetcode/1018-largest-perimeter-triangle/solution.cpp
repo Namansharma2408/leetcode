9aeaa60dcab6f6076dc2679db03ade5de25cdf9c
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-06-08 17:29:14 +0000

    Sync LeetCode submission Runtime - 3 ms (77.58%), Memory - 25.6 MB (76.27%)

---
+++ leetcode/1018-largest-perimeter-triangle/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        for( int i = n-2 ; i >= 1 ; i--){
+            if(nums[i-1] + nums[i] > nums[i+1] ) return (nums[i-1]+nums[i]+nums[i+1]);
+        }
+        return 0;
+    }
+};
