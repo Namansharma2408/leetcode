c76de2b7f23ec59a69b077e3f429bfd7f46cdc11
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-06-10 06:02:39 +0000

    Sync LeetCode submission Runtime - 166 ms (71.50%), Memory - 99.8 MB (84.98%)

---
+++ leetcode/1988-minimize-maximum-pair-sum-in-array/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int mini = nums[0] + nums[n-1];
+        for( int i = 1 ; i < n/2 ; i++){
+            mini = max(mini,nums[i]+nums[n-1-i]);
+        }
+        return mini;
+    }
+};
