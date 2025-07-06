46c426f903847f5d2a1632831e410f2cf9dacaff
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-07-06 19:26:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.2 MB (55.48%)

--- leetcode/0733-flood-fill/solution.cpp
+++ leetcode/0733-flood-fill/solution.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    void dfs( vector<vector<int>>& image, int sr, int sc, int color,int m , int n,int orignalcolor){
-        image[sr][sc] = color;
-        if( sr >= 1 && image[sr-1][sc] == orignalcolor) dfs(image,sr-1,sc,color,m,n,orignalcolor);    
-        if( sr < m-1 && image[sr+1][sc] == orignalcolor) dfs(image,sr+1,sc,color,m,n,orignalcolor);
-        if( sc >= 1 && image[sr][sc-1] == orignalcolor) dfs(image,sr,sc-1,color,m,n,orignalcolor);
-        if( sc < n-1 && image[sr][sc+1] == orignalcolor) dfs(image,sr,sc+1,color,m,n,orignalcolor);
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if( image[sr][sc] == color) return image;
         int m = image.size();
         int n = image[0].size();
-        int orignalcolor = image[sr][sc];
-        if( orignalcolor != color) dfs(image,sr,sc,color,m,n,orignalcolor);
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        int c = image[sr][sc];
+        image[sr][sc] = color;
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            image[p.first][p.second] = color;
+            if(p.first > 0 && c == image[p.first-1][p.second] ) {
+                q.push({p.first-1,p.second});
+                image[p.first-1][p.second] = color; 
+            }
+            if(p.second > 0 && c == image[p.first][p.second-1] ) {
+                q.push({p.first,p.second-1});
+                image[p.first][p.second-1] = color;   
+            }
+            if(p.first < m-1 && c == image[p.first+1][p.second] ) {
+                q.push({p.first+1,p.second});
+                image[p.first+1][p.second] = color;    
+            }
+            if(p.second < n-1 && c == image[p.first][p.second+1] ) {
+                q.push({p.first,p.second+1});
+                image[p.first][p.second+1] = color;  
+            }
+        }
         return image;
     }
 };
