6ce2abce8808cbed3130475a0b1ca0785fa94ea3
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-07-10 14:13:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.1 MB (61.41%)

---
+++ leetcode/0133-clone-graph/solution.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<Node*> nodeRegister;
+    void dfs(Node* actual,Node* clone){
+        for( auto neighbour: actual->neighbors){
+            if( !nodeRegister[neighbour->val]){
+                Node* newNode = new Node(neighbour->val);
+                nodeRegister[neighbour->val] = newNode;
+                clone->neighbors.push_back(newNode);
+                dfs(neighbour,newNode);
+            }else{
+                clone->neighbors.push_back(nodeRegister[neighbour->val]);
+            }
+        }
+    }
+    Node* cloneGraph(Node* node) {
+        if( node == NULL) return NULL;
+        Node* clone = new Node(node->val);
+        nodeRegister.resize(110,NULL);
+        nodeRegister[clone->val] = clone;
+        dfs(node,clone);
+        return clone;
+        
+    }
+};
