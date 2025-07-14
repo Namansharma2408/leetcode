84ce2f455973d545c9773592b68febaa58210adc
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-07-14 08:30:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.1 MB (31.35%)

---
+++ leetcode/0135-candy/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> v(n, 1);
+        for (int i = 0; i < n - 1; i++) {
+            if (ratings[i + 1] > ratings[i]) {
+                v[i + 1] = v[i] + 1;
+            }
+        }
+        int cnt = 0;
+        for (int i = n - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                v[i - 1] = max(v[i] + 1, v[i - 1]);
+            cnt += v[i - 1];
+        }
+        return cnt + v[n - 1];
+    }
+};
