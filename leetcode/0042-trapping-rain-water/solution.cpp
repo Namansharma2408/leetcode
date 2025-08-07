95aa583a53c165dc3c7ef5255b1ef1ce79337a7f
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-08-07 06:52:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.1 MB (58.34%)

--- leetcode/0042-trapping-rain-water/solution.cpp
+++ leetcode/0042-trapping-rain-water/solution.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        int maxidx = 0;
         int n = height.size();
-        int prev[n];
-        int next[n];
-        prev[0]= -1;
-        int max = height[0];
-        for(int i = 1; i< n; i++){
-          prev[i] = max;
-          if( max < height[i]) max = height[i];
-       }
-       prev[n-1] = -1;
-       max = height[n-1] ;
-       for( int i = n-2 ; i >= 0 ; i--){
-        if( max < prev[i]) prev[i] = max;
-        if( max < height[i]) max = height[i];
-       }
-        int water = 0;
-        for( int i = 1 ; i< n-1 ; i++){
-            if(height[i] < prev[i]){
-                water += (prev[i] - height[i]);
-            }
+        long long sum = 0;
+        for( int i = 0 ; i < n ; i++){
+            sum += height[i];
+            if(height[i] > height[maxidx]) maxidx = i;
         }
-        return water;
+        int maxi = 0;
+        for( int i = 0 ; i < maxidx ; i++){
+            if(height[i] > maxi ) maxi = height[i];
+            height[i] = maxi;
+        }
+        maxi = 0;
+        for( int i = n-1 ; i > maxidx ; i--){
+            if(height[i] > maxi ) maxi = height[i];
+            height[i] = maxi;
+        }
+        for( int i = 0 ; i < n ; i++){
+            sum -= height[i];
+        }
+        int ans = -sum;
+        return ans;
+        
+        
     }
 };
