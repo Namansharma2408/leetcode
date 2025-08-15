56a3fb2ec6e22e634b8b3026c31b16dfa86c7c7e
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-08-15 09:10:47 +0000

    Sync LeetCode submission Runtime - 38 ms (99.30%), Memory - 19.9 MB (99.76%)

--- leetcode/0160-intersection-of-two-linked-lists/solution.cpp
+++ leetcode/0160-intersection-of-two-linked-lists/solution.cpp
@@ -9,34 +9,29 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if( headA == headB ) return headA;
-        ListNode* tempa = headA;
-        ListNode* tempb = headB;
-        int sizea = 0;
-        int sizeb = 0;
-        while( tempa != NULL){
-            sizea++;
-            tempa = tempa->next;
+        stack<ListNode*> st1;
+        stack<ListNode*> st2;
+        ListNode* temp = headA;
+        while( temp ){
+            st1.push(temp);
+            temp = temp->next;
         }
-        while( tempb != NULL){
-            sizeb++;
-            tempb = tempb->next;
+        temp = headB;
+        while( temp ){
+            st2.push(temp);
+            temp = temp->next;
         }
-        tempa = headA,tempb = headB;
-        if( sizea > sizeb){
-            for( int i = 0 ; i < sizea-sizeb ; i++){
-                tempa = tempa->next;
-            }
+
+        // checking for the intersection
+        temp = NULL;
+        while( !st1.empty() && !st2.empty() ){
+            cout<<"hii"<<endl;
+            if( st1.top() == st2.top() ) temp = st1.top();
+            else break;
+            st1.pop();
+            st2.pop();
         }
-        else if( sizea < sizeb){
-            for( int i = 0 ; i < sizeb-sizea ; i++){
-                tempb = tempb->next;
-            }
-        }
-        while( tempa != tempb){
-            tempa = tempa->next;
-            tempb = tempb->next;
-        }
-        return tempa;
+        return temp;
+        
     }
 };
