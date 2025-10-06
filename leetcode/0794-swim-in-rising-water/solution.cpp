5367863bcb8678f75d68a511bb2640f0766a518c
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2025-10-06 02:08:44 +0000

    Sync LeetCode submission Runtime - 3 ms (88.38%), Memory - 12.7 MB (85.91%)

--- leetcode/0794-swim-in-rising-water/solution.cpp
+++ leetcode/0794-swim-in-rising-water/solution.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
-            pq;
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
         int maxi = grid[0][0];
-        pq.push({grid[0][0], {0, 0}});
-        int i = 0;
-        vector<int> dx = {0, 0, 1, -1};
-        vector<int> dy = {1, -1, 0, 0};
-        while (!pq.empty()) {
-            auto [val, pos] = pq.top();
-            maxi = max(maxi,val);
+        visited[0][0] = true;
+        pq.push({grid[0][0],{0,0}});
+        while( !pq.empty() ){
+            maxi = max(maxi,pq.top().first);
+            auto p = pq.top();
             pq.pop();
-            int x = pos.first, y = pos.second;
-            if ((x == m - 1) && (y == n - 1))
-                return maxi;
-
-            for (int d = 0; d < 4; ++d) {
-                int nx = x + dx[d], ny = y + dy[d];
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n &&
-                    !visited[nx][ny]) {
-                    pq.push({grid[nx][ny], {nx, ny}});
-                    visited[nx][ny] = true;
-                }
+            int r = p.second.first;
+            int c = p.second.second;
+            // cout<<" r-> "<<r<<" c-> "<<c<<" num => "<<p.first<<endl;
+            if( r == m-1 && c == n-1 ) return maxi;
+            if(  r >= 1 && !visited[r-1][c]){
+                visited[r-1][c] = true;
+                pq.push({grid[r-1][c],{r-1,c}});
+            }
+            if( r < m-1 && !visited[r+1][c]){
+                visited[r+1][c] = true;
+                pq.push({grid[r+1][c],{r+1,c}});
+            }
+            if( c >= 1 && !visited[r][c-1]){
+                visited[r][c-1] = true;
+                pq.push({grid[r][c-1],{r,c-1}});
+            }
+            if( c < n-1 && !visited[r][c+1]){
+                visited[r][c+1] = true;
+                pq.push({grid[r][c+1],{r,c+1}});
             }
         }
+
         return maxi;
     }
 };
