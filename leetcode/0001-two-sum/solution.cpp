61e59f84b1103c9b8057e343d0830dc109a05061
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2026-01-01 23:32:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.7 MB (60.71%)

--- leetcode/0001-two-sum/solution.cpp
+++ leetcode/0001-two-sum/solution.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        int n = nums.size();
-        unordered_map<int,int> map;
-        for( int i = 0 ; i < n ; i++){
-            int rem = target - nums[i];
-            if( map.find(rem) != map.end()){
-                ans.push_back(map[rem]);
-                ans.push_back(i);
-            }
-            else map[nums[i]] = i;
+        vector<int> v = nums;
+        sort(v.begin(),v.end());
+        int i = 0;
+        int j = v.size() - 1;
+        while( i < j ){
+            int x = v[i] + v[j];
+            if( x == target ) break;
+            else if( x > target ) j--;
+            else i++;
         }
+        vector<int> ans;
+        for( int k = 0 ; k < nums.size() ; k++ ){
+            if( nums[k] == v[i] || nums[k] == v[j] ){
+                ans.push_back(k);
+            }
+        }
+        if( ans[0] > ans[1] ) swap(ans[0],ans[1]);
         return ans;
     }
 };
