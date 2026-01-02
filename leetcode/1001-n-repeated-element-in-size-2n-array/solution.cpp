126e484af995c4038f0b55a2fe39eb6676982367
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2026-01-02 14:57:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.5 MB (44.24%)

---
+++ leetcode/1001-n-repeated-element-in-size-2n-array/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        for( int i = 0 ; i < n ; i++ ){
+            mp[nums[i]]++;
+            if( mp[nums[i]] > 1 ) return nums[i];
+        }
+        return -1;
+    }
+};
