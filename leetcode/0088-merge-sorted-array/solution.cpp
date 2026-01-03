4ec33f54571b9c577a2caaeb4ad88617078e9045
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2026-01-03 10:46:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (40.79%)

--- leetcode/0088-merge-sorted-array/solution.cpp
+++ leetcode/0088-merge-sorted-array/solution.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
-        while( k >= 0){
-            if( i >= 0 && j >= 0){
-                if( nums1[i] > nums2[j]){
-                    nums1[k] = nums1[i];
-                    i--;
-                }else {
-                    nums1[k] = nums2[j];
-                    j--;
-                }
-            }
-            else if( i < 0){
-                nums1[k] = nums2[j];
-                j--;
-            }
-            else if( j < 0){
-                nums1[k] = nums1[i];
-                i--;
-            }
-            k--;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2,int n){
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        
+        while( i >= 0 && j >= 0 ){
+            if( nums1[i] > nums2[j] ) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
         }
-
+        if( j >= 0 ){
+            for( int l = j ; l >= 0 ; l-- ){
+                nums1[l] = nums2[j--];
+            }
+        }
     }
 };
