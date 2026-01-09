d2e98d3dd79ceb567398464ff861bf964b841982
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2026-01-09 23:09:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.3 MB (36.33%)

--- leetcode/0048-rotate-image/solution.cpp
+++ leetcode/0048-rotate-image/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    void transpose(vector<vector<int>>& matrix,int n){
-        for( int i = 1 ; i < n ; i++){
-            for( int j = 0 ; j <= i ; j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
-        }
-    }
-    void horizontalReverse(vector<vector<int>>& matrix,int n){
-        for( int i = 0 ; i < n ; i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
-    }
 public:
-    
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        transpose(matrix,n);
-        horizontalReverse(matrix,n);
+        for( int i = 0 ; i < n ; i++ ){
+            for( int j = i+1 ; j < n ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for( int i = 0 ; i < n ; i++ ){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+
+        return;
     }
 };
