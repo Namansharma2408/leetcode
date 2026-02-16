b2ba949380b5193d1fc0230755910b0783b01e67
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2026-02-16 11:07:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (72.95%)

--- leetcode/0190-reverse-bits/solution.cpp
+++ leetcode/0190-reverse-bits/solution.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
-        uint32_t rev_n= 0;
-        for(int i = 0 ; i < 32 ; i++){
-            rev_n *= 2;
-            rev_n += n%2;
+    int reverseBits(int n) {
+        int ans = 0;
+        int i = 0;
+        while( i < 32 ){
+            ans *= 2;
+            ans += n%2;
             n /= 2;
+            i++;
         }
-        return rev_n;
+        return ans;
+        
     }
 };
