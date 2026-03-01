5e0cd8611f1f7fcd21ca290f2098a26dfde05241
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2026-03-01 06:51:18 +0000

    Sync LeetCode submission Runtime - 3 ms (39.44%), Memory - 16.6 MB (56.70%)

--- leetcode/1807-partitioning-into-minimum-number-of-deci-binary-numbers/solution.cpp
+++ leetcode/1807-partitioning-into-minimum-number-of-deci-binary-numbers/solution.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        int size = n.size();
-        int maxi = -1;
-        for( int i = 0 ; i < size ; i++) maxi = max(maxi,(n[i]-48));
-        return maxi;
+        char maxi = INT_MIN;
+        int s = n.size();
+        for( int i = 0 ; i < s ; i++ ){
+            maxi = max(maxi,n[i]);
+        }
+        return int(maxi-'0');
     }
 };
