e1d420ae50ef4c2b1013fedf1084851b4924e146
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2026-03-08 05:29:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13 MB (42.93%)

--- leetcode/2107-find-unique-binary-string/solution.cpp
+++ leetcode/2107-find-unique-binary-string/solution.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<string> st(nums.begin(),nums.end());
-        int n = nums.size();
-        string str(n,'0');
-        if( st.find(str) == st.end()) return str;
-        for( int i = 0 ; i < n ; i++){
-            str[i] = '1';
-            if( st.find(str) == st.end()) return str;
+    void back(unordered_set<string> &st,string &str,int n, bool &b){
+        if( str.size() == n ){
+            if( st.find(str) == st.end() ){
+                b = true;
+            }
+            return;
         }
+        if( b == true ) return;
+        str.push_back('0');
+        back(st,str,n,b);
+        if( b == true ) return;
+        str.pop_back();
+        str.push_back('1');
+        back(st,str,n,b);
+        if( b == true ) return;
+        str.pop_back();
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        int m = nums.size();
+        int n = nums[0].size();
+        bool b = false;
+        unordered_set<string> st(nums.begin(),nums.end());
+        string str;
+        back(st,str,n,b);
         return str;
 
     }
