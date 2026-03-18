e8b353adb9080e0d1e31361ba22ff1d56c9d06a6
Naman Sharma <149923207+Namansharma2408@users.noreply.github.com>
2026-03-18 06:09:34 +0000

    Sync LeetCode submission Runtime - 10 ms (62.72%), Memory - 90.6 MB (65.95%)

--- leetcode/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/solution.cpp
+++ leetcode/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int ans = 0;
         int m = grid.size();
         int n = grid[0].size();
-        for( int i = 1 ; i < n ; i++) grid[0][i] += grid[0][i-1];
-        for( int i = 1; i < m ; i++){
-            vector<int> v = grid[i];
-            for( int j = 1 ; j < n ; j++){
-                grid[i][j] += grid[i-1][j] + v[j-1];
-                v[j] += v[j-1];
-            }
-        }
-        for( int i = 1 ; i < m ; i++) grid[i][0] += grid[i-1][0];
-        int ans = 0;
-        for( int i = 0; i < m ; i++){
-            for( int j = 0 ; j < n ; j++){
-                if( grid[i][j] <= k) ans++;
+        for( int i = 0 ; i < m ; i++ ){
+            for( int j = 0 ; j < n ; j++ ){
+                if( i > 0 ) grid[i][j] += grid[i-1][j];
+                if( j > 0 ) grid[i][j] += grid[i][j-1];
+                if( j > 0 && i > 0 ) grid[i][j] -= grid[i-1][j-1];
+                if( grid[i][j] <= k ) {
+                    ans++;
+                }
+                if( grid[i][j] > k ) break;
             }
         }
         return ans;
